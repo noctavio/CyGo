a353fa3ccdc89a5743ca1a1eca3144e090d2ec2d
me <@noctavio>
2024-09-11 02:38:54 -0500

    changes to experiment-1

--- Experiments/Experiments-Octavio/experiment-1/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/Experiments-Octavio/experiment-1/helloworld/src/main/java/coms309/WelcomeController.java
@@ -9,11 +9,11 @@ class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "Hello and welcome to COMS 309";
+        return "Hello and welcome to COMS 309, you are currently reading the default message displayed when no string is specified after the url {'/string'}";
     }
 
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
-        return "Hello and welcome to COMS 309: " + name;
+        return "Hello and welcome to COMS 3090, previously known as COM S 309: " + name;
     }
 }
