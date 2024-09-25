19dbe48bc939908f693ce4933c15a3764c7bff48
me <@noctavio>
2024-09-25 00:50:13 -0500

    test code, maven spitting errors until properly configured

---
+++ Backend/User-Login-Registration/login-backend/src/main/java/com/example/login_backend/UserService.java
@@ -0,0 +1,40 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public User register(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User updateUser(Long id, User userDetails) {
+        User user = getUserById(id);
+        if (user != null) {
+            user.setUsername(userDetails.getUsername());
+            user.setPassword(passwordEncoder.encode(userDetails.getPassword()));
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
