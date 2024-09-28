180f7b3d718a39d92347facfa2baea1d5856a90e
me <@noctavio>
2024-09-28 04:55:52 -0500

    4:55 am commit trying to fix an issue for 12 hours straight now :)

--- Backend/User-Login-Registration/login-backend/src/main/java/com/example/login/User.java
+++ Backend/User-Login-Registration/login-backend/src/main/java/com/example/login/User.java
@@ -1,7 +1,12 @@
 package com.example.login;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "users")
 public class User {
@@ -14,12 +19,4 @@ public class User {
 
     private String password;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
 }
--- Backend/User-Login-Registration/login-backend/src/main/java/com/example/login/controller/TestController.java
+++ Backend/User-Login-Registration/login-backend/src/main/java/com/example/login/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.login.controller; // Make sure to match your package structure
+package com.example.login.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
