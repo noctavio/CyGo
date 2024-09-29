9f104b9c6dd11fe2c61b37af4a6ca1300627d6c4
me <@noctavio>
2024-09-29 02:39:13 -0500

    connected to the remote server

--- Backend/User-Login-Registration/login-backend/src/main/java/com/example/login/Controller/UserController.java
+++ Backend/User-Login-Registration/login-backend/src/main/java/com/example/login/Controller/UserController.java
@@ -3,11 +3,12 @@ package com.example.login.Controller;
 import com.example.login.Entity.User;
 import com.example.login.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
+@RequestMapping("/users")
 @RestController
 public class UserController {
     @Autowired
@@ -15,6 +16,19 @@ public class UserController {
 
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
-        return userService.updateUser(user);
+        return userService.registerUser(user);
     }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable int id) {
+        User user = userService.getUserById(id);
+        return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
+    }
+
+
 }
--- Backend/User-Login-Registration/login-backend/src/main/java/com/example/login/Service/UserService.java
+++ Backend/User-Login-Registration/login-backend/src/main/java/com/example/login/Service/UserService.java
@@ -3,15 +3,28 @@ package com.example.login.Service;
 import com.example.login.Entity.User;
 import com.example.login.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User updateUser(User user) {
+    public User registerUser(User user) {
+        String secret = user.getPassword();
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        user.setPassword(encoder.encode(secret));
+
         return userRepository.save(user);
+    }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    public User getUserById(int id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
