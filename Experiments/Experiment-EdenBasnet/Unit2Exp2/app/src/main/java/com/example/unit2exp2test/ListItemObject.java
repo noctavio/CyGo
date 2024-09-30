3594adf7e9ab63079d4ef357c762718e42f051dd
Eden Basnet <ebasnet@iastate.edu>
2024-09-29 21:53:25 -0500

    Adding Unit2Exp2 and Unit2Exp3 files

---
+++ Experiments/Experiment-EdenBasnet/Unit2Exp2/app/src/main/java/com/example/unit2exp2test/ListItemObject.java
@@ -0,0 +1,22 @@
+package com.example.unit2exp2test;
+
+
+public class ListItemObject {
+    private String name;
+    private String email;
+
+    public ListItemObject(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
+
