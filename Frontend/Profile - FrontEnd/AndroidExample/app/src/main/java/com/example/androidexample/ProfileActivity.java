65b4d2b0be382e2fd0eb2aed07b67f18a91ac20f
Matt <matthill@iastate.edu>
2024-10-07 14:37:15 -0500

    Final Fix on Leaderboar

---
+++ Frontend/Profile - FrontEnd/AndroidExample/app/src/main/java/com/example/androidexample/ProfileActivity.java
@@ -0,0 +1,12 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+}
