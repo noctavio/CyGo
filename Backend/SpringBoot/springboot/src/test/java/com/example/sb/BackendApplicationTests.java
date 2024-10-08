e9c8e1f5b2718cc5ff71b31ea60590654c3cc1ea
me <@noctavio>
2024-10-07 20:16:49 -0500

    Finalizing roundtrip, directory errors fixed, stable version

--- Backend/SpringBoot/springboot/src/test/java/com/example/sb/BackendApplicationTests.java
+++ Backend/SpringBoot/springboot/src/test/java/com/example/sb/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.login;
+package com.example.sb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LoginBackendApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
