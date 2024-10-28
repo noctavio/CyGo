9339ef89bab42921c6fb9b7a827b57fdbd04bc52
Devonk5 <keuningdevon@gmail.com>
2024-10-28 16:01:56 -0500

    club patches

--- Backend/SpringBoot/springboot/src/main/java/com/example/sb/Model/Club.java
+++ Backend/SpringBoot/springboot/src/main/java/com/example/sb/Model/Club.java
@@ -1,71 +1,35 @@
 package com.example.sb.Model;
 
-import com.example.sb.Entity.User;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.Id;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name = "clubs")
-
 @AllArgsConstructor
 @NoArgsConstructor
-public class
-Club {
+public class Club {
 
     @Getter
     @Setter
     @jakarta.persistence.Id
-    @Id()
     @Column(name = "ID")
-    @GeneratedValue
-    private int id; // this is a primary key as annotated by @Id() may change/delete in the future
-    // Foreign key mapping to (registered_users)
-    
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id; // Primary key
+
     @Column(name = "club_name")
-    private String clubname;
+    private String clubName;
+
     @Column(name = "club_picture")
-    private String clubpicture;
-    @Column(name = "member1")
-    private Integer member1;
-    @Column(name = "member2")
-    private Integer member2;
-    @Column(name = "member3")
-    private Integer member3;
-    @Column(name = "member4")
-    private Integer member4;
-    @Column(name = "member5")
-    private Integer member5;
-    @Column(name = "member6")
-    private Integer member6;
-    @Column(name = "member7")
-    private Integer member7;
-    @Column(name = "member8")
-    private Integer member8;
-    @Column(name = "member9")
-    private Integer member9;
-    @Column(name = "member10")
-    private Integer member10;
-    @Column(name = "member11")
-    private Integer member11;
-    @Column(name = "member12")
-    private Integer member12;
-    @Column(name = "member13")
-    private Integer member13;
-    @Column(name = "member14")
-    private Integer member14;
-    @Column(name = "member15")
-    private Integer member15;
-    @Column(name = "member16")
-    private Integer member16;
-    @Column(name = "member17")
-    private Integer member17;
-    @Column(name = "member18")
-    private Integer member18;
-    @Column(name = "member19")
-    private Integer member19;
-    @Column(name = "member20")
-    private Integer member20;
+    private String clubPicture;
+
+    @ElementCollection
+    @CollectionTable(name = "club_members", joinColumns = @JoinColumn(name = "club_id"))
+    @Column(name = "member_name")
+    private List<String> members = new ArrayList<>(); // List of member names
+
 }
 
