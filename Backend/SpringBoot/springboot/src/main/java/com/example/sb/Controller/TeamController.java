acddb7f6c5afdb22125e9a0faf4734c771e50c61
me <@noctavio>
2024-11-02 05:09:46 -0500

    controller mapping name change

--- Backend/SpringBoot/springboot/src/main/java/com/example/sb/Controller/TeamController.java
+++ Backend/SpringBoot/springboot/src/main/java/com/example/sb/Controller/TeamController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/lobby/team")
+@RequestMapping("/lobby/teams")
 @RestController
 public class TeamController {
     @Autowired
